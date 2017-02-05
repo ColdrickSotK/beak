743d173a3c0b790d54b6b0c1a95451684a1e58d6
Adam Coldrick <adam@sotk.co.uk>
2017-02-05 14:15:28 +0000

    Add a POST endpoint to handle the request from the editor

--- beak.py
+++ beak.py
@@ -15,13 +15,62 @@
 """Beak flask app for serving the API"""
 
 import os
+import time
 
 from flask import Flask
 from flask import render_template
+from flask import request
 
 
 app = Flask(__name__)
 
-@app.route('/posts/create')
-def new_post():
+
+def write_metadata(post, orig_date=None):
+    post.write('Title: %s\n' % request.form['title'])
+
+    if orig_date is None:
+        post.write('Date: %s\n' % time.strftime('%Y-%m-%d %H:%M'))
+    else:
+        post.write('Date: %s\n' % orig_date)
+    post.write('Modified: %s\n' % time.strftime('%Y-%m-%d %H:%M'))
+
+    if request.form.get('category'):
+        post.write('Category: %s\n' % request.form['category'])
+    else:
+        post.write('Category: Misc\n')
+
+    post.write('Tags: %s\n' % request.form['tags'])
+    post.write('Authors: %s\n' % request.form['author'])
+
+    if request.form.get('filename'):
+        post.write('Slug: %s\n' % request.form['filename'])
+    if request.form.get('summary'):
+        post.write('Summary: %s\n' % request.form['summary'])
+
+
+def add_post_from_request():
+    post_dir = os.path.join('posts', time.strftime('%Y-%m-%d'))
+    if not os.path.exists(post_dir):
+        os.mkdir(post_dir)
+
+    post_path = os.path.join(
+        post_dir, request.form.get('filename', 'post') + '.md')
+
+    orig_date = None
+    if os.path.exists(post_path):
+        with open(post_path, 'r') as post:
+            for line in post.readlines():
+                if line.startswith('Date: '):
+                    orig_date = line[6:].strip()
+                    break
+
+    with open(post_path, 'w') as post:
+        write_metadata(post, orig_date)
+        post.write('\n%s' % request.form['content'])
+
+
+@app.route('/posts/create', methods=['GET', 'POST'])
+def create_post():
+    if request.method == 'POST':
+        add_post_from_request()
     return render_template('create.html')
